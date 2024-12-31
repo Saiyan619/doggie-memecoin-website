6d010403753e5831f09c50c7d8387d82529783f9
olaniyi <arokoyueb11@gmail.com>
2024-12-31 15:26:33 +0100

    adding new section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import SectionTwo from './section2/SectionTwo'
 import SectionThree from './section3/SectionThree'
 import SectionFour from './section4/SectionFour'
 import SectionSix from './section6/SectionSix'
+import SectionSeven from './section7/SectionSeven'
 import SectionFive from './sectionfive/SectionFive'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       <SectionFour />
       <SectionFive />
       <SectionSix />
+      <SectionSeven />
     </div>
   )
 }
